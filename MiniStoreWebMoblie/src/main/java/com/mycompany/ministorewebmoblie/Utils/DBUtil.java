8c1f6de907705c5764cbea9e2678065014a37c4b
BlackerDK <duykhanhz1000st@gmail.com>
2023-07-12 17:15:27 +0700

    Update first

---
+++ MiniStoreWebMoblie/src/main/java/com/mycompany/ministorewebmoblie/Utils/DBUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ministorewebmoblie.Utils;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author DUY KHANH
+ */
+public class DBUtil {
+    public static Connection makeConnection() {
+        Connection conn = null;
+        try {
+
+            String dbURL = "jdbc:sqlserver://BLACKER-DK\\SQLEXPRESS;database=BooksStore;encrypt=true;trustServerCertificate=true";
+            String user = "sa";
+            String pass = "12345";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            //System.out.println("Connect to DB successfully");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn){
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) throws SQLException {  
+        System.out.println("This is to test if we can connect to SQLServer");
+        Connection conn = makeConnection();
+        DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+        System.out.println("Driver name: " + dm.getDriverName());
+        System.out.println("Driver version: " + dm.getDriverVersion());
+        closeConnection(conn);
+    }
+}

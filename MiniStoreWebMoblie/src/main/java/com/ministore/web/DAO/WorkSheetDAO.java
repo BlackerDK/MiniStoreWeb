982ad3ca04a50e02155fcda48ef6b0203091316b
BlackerDK <duykhanhz1000st@gmail.com>
2023-07-13 08:00:15 +0700

    Create new package DAO and class WorksheetDAO

---
+++ MiniStoreWebMoblie/src/main/java/com/ministore/web/DAO/WorkSheetDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ministore.web.DAO;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author DUY KHANH
+ */
+public class WorkSheetDAO implements Serializable{
+    
+}
